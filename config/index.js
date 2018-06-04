4bb101abea2e79df143b29156a6939bea1b0c2f4
RongxiangWang <550995259@qq.com>
2018-06-04 20:34:37 +0800

    路径

--- config/index.js
+++ config/index.js
@@ -48,7 +48,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'https://wangrx-jerry.github.io/vue-imgUpload/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
